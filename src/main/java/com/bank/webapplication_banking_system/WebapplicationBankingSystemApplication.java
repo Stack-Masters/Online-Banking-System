716f57436b5251aa767b3d5cccf84a91159ca5ba
Thembinkosi Dladla <thembinkosidladla318@gmail.com>
2025-04-09 11:48:42 +0200

    Project set up with spring-boot

---
+++ src/main/java/com/bank/webapplication_banking_system/WebapplicationBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bank.webapplication_banking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapplicationBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapplicationBankingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/webapplication_banking_system/WebapplicationBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.webapplication_banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapplicationBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
